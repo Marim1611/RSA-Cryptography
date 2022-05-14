0d8e5fb8db742de3f318ba1322100c474855edd7
Marim1611 <marimnaser813@gmail.com>
2022-05-14 14:18:57 +0200

    effeciency computed  & plotting is done

--- main_program.py
+++ main_program.py
@@ -3,6 +3,8 @@
 import common_functions as cf
 import sender
 import receiver
+import time
+import matplotlib.pyplot as plt
 #--------------------- the two communicating parties ----------------------
 my_sender=sender.Sender()
 my_receiver= receiver.Receiver()
@@ -10,7 +12,9 @@ my_receiver= receiver.Receiver()
 test_file = open("test_cases.txt", "r")
 lines = test_file.read().splitlines()
 i =0
-while i < len(lines):
+encryption_time=[]
+key_length=[]
+while i < len(lines)-1:
     # read the message and the public key
     p=int(lines[i])
     q=int(lines[i+1])
@@ -24,9 +28,9 @@ while i < len(lines):
         print(" p and q must be primes")
         exit()
     #check that phi(n) and e are co-primes
-    if not(cf.areCoprime((p-1)*(q-1) ,e)):
-        print(" e and phi of n are not co-prime")
-        exit()    
+    # if not(cf.areCoprime((p-1)*(q-1) ,e)):
+    #     print(" e and phi of n are not co-prime")
+    #     exit()    
     #set values of the public key
     my_receiver.p=p
     my_receiver.q=q
@@ -34,13 +38,28 @@ while i < len(lines):
     # receiver sends its public key to the sender
     my_sender.set_public_key(p,q,e)
     #send the message to the sender to encrypt it
+    start_time=time.time()
     cipher_text= my_sender.encrypt(message)
+    end_time=time.time()
+    encryption_time.append(end_time- start_time)
+    key_length.append(len(bin(p*q).replace("0b", "")))
     print("------------------ cipher text ----------------")
     print(cipher_text)
     # send the cipher text to the receiver 
     decrypted_message= my_receiver.decrypt(cipher_text)
     print("------------------ decrypted message ----------------")
     print(decrypted_message)
+    print("~~~~~~~~~~~~~~~~~~~~ encryption time: " ,end_time-start_time)
+    print("~~~~~~~~~~~~~~~~~~~~ key length: " ,len(bin(p*q).replace("0b", "")))
     print("________________________________________________________________")
+# print(encryption_time)
+# print(key_length)    
 test_file.close() # close the file
+# plotting the key length VS encryption time (efficiency)
+plt.plot(encryption_time, key_length)
+plt.xlabel('encryption time')
+plt.ylabel('key length in bits')
+# # giving a title to my graph
+# plt.title('My first graph!')
+plt.show()
  
