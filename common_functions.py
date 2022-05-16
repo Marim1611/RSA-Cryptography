3b5d19976b08e569e73de352617b9b803931741d
Marim1611 <marimnaser813@gmail.com>
2022-05-16 13:43:47 +0200

    generate e randomly & more common functions

--- common_functions.py
+++ common_functions.py
@@ -1,3 +1,4 @@
+import random
 def listToString(s): 
         # initialize an empty string
         str1 = "" 
@@ -25,19 +26,62 @@ def isPrime(num):
     else:
         return False
 def gcd(a, b):
-    if (a == 0 or b == 0): return 0
-     
-    # base case
-    if (a == b): return a
-     
-    # a is greater
-    if (a > b):
-        return gcd(a - b, b)         
-    return gcd(a, b - a)
-
+    if b == 0:
+        return a
+    return gcd(b, a%b)    
+    # if (a == 0 or b == 0): return 0
+    # # base case
+    # if (a == b): return a
+    # # a is greater
+    # if (a > b):
+    #     return gcd(a % b, b)         
+    # return gcd(a, b % a)
 # two numbers are co-prime or not
 def areCoprime(a, b):  
     if (gcd(a, b) == 1):
         return True
     else:
-        return False           
+        return False    
+def ConvertToStr(n):
+    res = ""
+    while n > 0:
+        res += chr(n % 256)
+        n //= 256
+    return res[::-1]
+def ConvertToInt(message_str):
+    res = 0
+    for i in range(len(message_str)):
+        res = res * 256 + ord(message_str[i])
+    return res
+def PowMod(a, n, mod):
+    if n == 0:
+        return 1 % mod
+    elif n == 1:
+        return a % mod
+    else:
+        b = PowMod(a, n // 2, mod)
+        b = b * b % mod
+    if n % 2 == 0:
+        return b
+    else:
+        return b * a % mod 
+
+def ExtendedEuclid(a, b):
+    if b == 0:
+        return (1, 0)
+    (x, y) = ExtendedEuclid(b, a % b)
+    k = a // b
+    return (y, x - k * y)
+               
+def modInverse(a, n):
+    (b, x) = ExtendedEuclid(a, n)
+    if b < 0:
+        b = (b % n + n) % n # we don't want -ve integers
+    return b   
+
+def generate_e(phi_n): 
+    e = random.randint(1,phi_n)  
+    while not areCoprime(e, phi_n):
+       e = random.randint(1,phi_n)
+    return e   
+
