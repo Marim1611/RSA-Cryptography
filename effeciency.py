c53fb0c3484e2f0ff99cf191d29bea6828d9e65b
Marim1611 <marimnaser813@gmail.com>
2022-05-17 18:34:38 +0200

    updated encyption time vs key length plot

---
+++ effeciency.py
@@ -0,0 +1,43 @@
+import random
+import common_functions as cf
+import senderClass as s
+import time
+import matplotlib.pyplot as plt
+#--- 64 => 20 digits
+#
+
+theSender= s.Sender()
+n= 64
+key_lengths=[]
+encryption_time=[]
+message="hello"
+test_file = open("P_Q.txt", "r")
+lines = test_file.read().splitlines()
+i =0
+while i < len(lines)-1:
+    # read the message and the public key
+    p=int(lines[i])
+    q=int(lines[i+1])
+    key_lengths.append(len(bin(p*q).replace("0b", "")))
+    i+=3
+    e= cf.generate_e( (p-1) * (q-1)) 
+    print("E")   
+    print(e)
+    theSender.set_public_key(p,q,e)
+    start_time=time.time()
+    cipher_text=theSender.encrypt(message) 
+    end_time=time.time()
+    print(cipher_text)
+    encryption_time.append( end_time - start_time)
+test_file.close() # close the file   
+print(encryption_time)
+# plotting the key length VS encryption time (efficiency)
+plt.plot(key_lengths,encryption_time )
+plt.xlabel('encryption time')
+plt.ylabel('key length in bits')
+# # giving a title to my graph
+plt.title('RSA')
+plt.show()
+    
+
+
