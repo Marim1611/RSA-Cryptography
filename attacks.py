e7e6c024cbe2ad056647784bdbe11cdd5b1304b9
Marim1611 <marimnaser813@gmail.com>
2022-05-19 01:06:50 +0200

    clean the code

--- attacks.py
+++ attacks.py
@@ -1,7 +1,9 @@
 import common_functions as cf
 import receiverClass as r
 import senderClass as s
-msg="hello"
+
+msg=input("enter message")
+type= input("MA or CCA ?")
 # Bob (receiver) decryption
 Bob= r.Receiver()
 Bob_data = open("pq.txt", "r")
@@ -18,14 +20,51 @@ e= Bob.e
 Alice = s.Sender()
 Alice.set_public_key( Bob.p, Bob.q , Bob.e)
 cipher_text = Alice.encrypt(msg)
+C=cf.ConvertToInt(cipher_text)
 
-C= Alice.encrypt(msg)
 #write data in file that attacker will intercept
 with open('attack_test.txt', 'w') as f:
-    f.write(C + "\n")
+    f.write(str(C)+ "\n")
     f.write(str(Bob.e) + "\n")
-    f.write(str(Bob.p*Bob.q) + "\n")
-f.close()     
+    f.write(str(Bob.p*Bob.q))
+f.close()    
+
+#------------------------------ CCA ATTACK -----------------
+# Alice to Bob: M ====> C
+#interested in finding M given C,e,n
+# Eve intercepts C 
+if (type == "CCA"):
+    attacker_data = open("attack_test.txt", "r")
+    lines = attacker_data.read().splitlines()
+    i =0
+    while i < len(lines)-1:
+        C=int(lines[i])
+        e=int(lines[i+1])
+        n=int(lines[i+2])
+        i+=4
+    attacker_data.close()
+    #intercepted cipher text
+    # generate r that is co-prime with el n
+    r = cf.generate_e(n) 
+    C_dash=C* cf.PowMod( r,e,n)
+    # Bob decrypt C dash 
+    Y = cf.ConvertToInt( Bob.decrypt( cf.ConvertToStr( C_dash)))
+    M= cf.PowMod( (Y* cf.modInverse(r,n)),1,n)
+    recovered= cf.ConvertToStr(M)
+    #write attack results and original message
+    with open('CCA_results.txt', 'w') as f:
+        f.write(msg+ "\n")
+        f.write(recovered + "\n")
+        f.close()
+#--------------------- Mathematical Attack ----------------------------------------  
+elif (type == "MA"):
+    pass
+else:
+    print("please choose CCA or MA")
+ 
+    
+
+
 
 
 
