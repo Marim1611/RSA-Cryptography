2d2f234003b7a384d374661b9e621073af6c74bd
mariemzayn18 <mariamzayn18@gmail.com>
2022-05-19 11:32:18 +0200

    MA

--- attacks.py
+++ attacks.py
@@ -2,8 +2,39 @@ import common_functions as cf
 import receiverClass as r
 import senderClass as s
 
-msg=input("enter message")
-type= input("MA or CCA ?")
+def MA(C, n, e):
+    recovered = ''
+    Eve = r.Receiver()
+    for p in range(2, int((n**0.5)+1)):
+        if(n % p == 0):
+            Eve.q = n//p
+            Eve.e = e
+            Eve.p = p
+            recovered = Eve.decrypt(cf.ConvertToStr(C))
+
+    return recovered
+
+def CCA(C, n, e):
+    
+    #intercepted cipher text
+    # generate r that is co-prime with el n
+    r = cf.generate_e(n) 
+    C_dash=C* cf.PowMod( r,e,n)
+    # Bob decrypt C dash 
+    Y = cf.ConvertToInt( Bob.decrypt( cf.ConvertToStr( C_dash)))
+    M= cf.PowMod( (Y* cf.modInverse(r,n)),1,n)
+    recovered= cf.ConvertToStr(M)
+
+    return recovered
+
+
+
+msg=input("Enter message: ")
+type= input("For MA press 1, For CCA press 2: ")
+
+# Alice to Bob: M ====> C
+#interested in finding M given C,e,n
+# Eve intercepts C 
 # Bob (receiver) decryption
 Bob= r.Receiver()
 Bob_data = open("pq.txt", "r")
@@ -29,49 +60,44 @@ with open('attack_test.txt', 'w') as f:
     f.write(str(Bob.p*Bob.q))
 f.close()    
 
+
+attacker_data = open("attack_test.txt", "r")
+lines = attacker_data.read().splitlines()
+i =0
+while i < len(lines)-1:
+    C=int(lines[i])
+    e=int(lines[i+1])
+    n=int(lines[i+2])
+    i+=4
+attacker_data.close()
+
+#---------------------------- Mathematical Attack --------------  
+if (type == "1"):
+
+    recovered= MA(C, n, e)
+    
+    #write attack results and original message
+    with open('MA_results.txt', 'w') as f:
+        f.write("Original message: "+ msg+ "\n")
+        f.write("Recovered message: "+ recovered + "\n")
+        f.close()
+
+    print("The attack is done, hard luck next time!")
+
+
 #------------------------------ CCA ATTACK -----------------
-# Alice to Bob: M ====> C
-#interested in finding M given C,e,n
-# Eve intercepts C 
-if (type == "CCA"):
-    attacker_data = open("attack_test.txt", "r")
-    lines = attacker_data.read().splitlines()
-    i =0
-    while i < len(lines)-1:
-        C=int(lines[i])
-        e=int(lines[i+1])
-        n=int(lines[i+2])
-        i+=4
-    attacker_data.close()
-    #intercepted cipher text
-    # generate r that is co-prime with el n
-    r = cf.generate_e(n) 
-    C_dash=C* cf.PowMod( r,e,n)
-    # Bob decrypt C dash 
-    Y = cf.ConvertToInt( Bob.decrypt( cf.ConvertToStr( C_dash)))
-    M= cf.PowMod( (Y* cf.modInverse(r,n)),1,n)
-    recovered= cf.ConvertToStr(M)
+
+elif (type == "2"):
+   
+    recovered= CCA(C, n, e)
+
     #write attack results and original message
     with open('CCA_results.txt', 'w') as f:
-        f.write(msg+ "\n")
-        f.write(recovered + "\n")
+        f.write("Original message: "+ msg+ "\n")
+        f.write("Recovered message: "+ recovered + "\n")
         f.close()
-#--------------------- Mathematical Attack ----------------------------------------  
-elif (type == "MA"):
-    pass
+
+    print("The attack is done, hard luck next time!")
+
 else:
-    print("please choose CCA or MA")
- 
-    
-
-
-
-
-
-
- 
-    
-
-
-
-    
+    print("please choose 1 or 2")
