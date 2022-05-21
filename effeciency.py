2a35fc3e54b13c48a1e5e019c5d460386c791ff2
Marim1611 <marimnaser813@gmail.com>
2022-05-22 00:22:39 +0200

    msg in effecincy graph is read now from input file

--- effeciency.py
+++ effeciency.py
@@ -3,14 +3,16 @@ import common_functions as cf
 import senderClass as s
 import time
 import matplotlib.pyplot as plt
-#--- 64 => 20 digits
-#
 
 theSender= s.Sender()
-n= 64
 key_lengths=[]
 encryption_time=[]
-message="hello"
+# read message to be encrypted
+test_file = open("eff_msg.txt", "r")
+lines = test_file.read().splitlines()
+message=lines[0]
+test_file.close() # close the file  
+# read Os & Qs being used to plot the graph     
 test_file = open("P_Q.txt", "r")
 lines = test_file.read().splitlines()
 i =0
@@ -20,23 +22,24 @@ while i < len(lines)-1:
     q=int(lines[i+1])
     key_lengths.append(len(bin(p*q).replace("0b", "")))
     i+=3
+    #generate public key e
     e= cf.generate_e( (p-1) * (q-1)) 
-    print("E")   
-    print(e)
     theSender.set_public_key(p,q,e)
+    #encrypt the message 
     start_time=time.time()
     cipher_text=theSender.encrypt(message) 
     end_time=time.time()
-    print(cipher_text)
+    # store time taken
     encryption_time.append( end_time - start_time)
-test_file.close() # close the file   
+test_file.close() # close the file 
+print("time taken:")  
 print(encryption_time)
+print("corresponding key lengths in bits:")  
 print(key_lengths)
 # plotting the key length VS encryption time (efficiency)
 plt.plot(key_lengths,encryption_time )
 plt.xlabel('key length in bits')
 plt.ylabel('encryption time')
-# # giving a title to my graph
 plt.title('RSA efficiency')
 plt.show()
     
