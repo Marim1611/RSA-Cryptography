e584ffe8521294412cf0c73c539ff8cbe1718d43
mariemzayn18 <mariamzayn18@gmail.com>
2022-05-21 23:50:11 +0200

    .

--- common_functions.py
+++ common_functions.py
@@ -48,7 +48,18 @@ def power_mod_solve(x, y, n):
         return b * x % n 
 
 def isPrime(n):
-    return sympy.isprime(n)
+    if n == 1 or n == 4:
+        return False
+    elif n == 2 or n == 3:
+        return True
+
+    for i in range(10000):
+        a = random.randint(2, n - 2)
+
+        if power_mod_solve(a, n - 1, n) != 1:
+            return False
+
+    # return sympy.isprime(n)
 
 
 def extended_euclidean_algo(a, b):
